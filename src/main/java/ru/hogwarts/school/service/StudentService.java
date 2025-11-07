b141aab5854e1ad25005919036f8fdb1ac3bf3d5
MaryPV <fornewstudies@gmail.com>
2025-11-07 21:51:14 +0500

    Connecting to the database

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositories.StudentRepository;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class StudentService {
+    private StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Collection<Student> getStudentsByAge(int age) {
+            if (age <= 0) {
+                return Collections.emptyList();
+            }
+            return studentRepository.findByAge(age);
+        }
+
+}
