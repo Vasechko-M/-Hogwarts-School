5fc09dcda6f37aa5cabeb700ed3ac827d518d728
MaryPV <fornewstudies@gmail.com>
2025-12-16 15:35:39 +0500

    Добавление индекса color. Замена includeAll на отдельные include

--- src/main/resources/liquibase/scripts/faculties.sql
+++ src/main/resources/liquibase/scripts/faculties.sql
@@ -1,3 +1,5 @@
 -- liquibase formatted sql
 -- changeset mvasechko:2
 create index faculty_name_index on faculty (name);
+-- changeset mvasechko:3
+create index faculty_color_index on faculty (color);
