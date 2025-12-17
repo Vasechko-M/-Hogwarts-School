4e75b106da1051af711a9d77a4bdd5fc79b08749
MaryPV <fornewstudies@gmail.com>
2025-12-17 16:00:13 +0500

    Объединение индекса name,color

--- src/main/resources/liquibase/scripts/faculties.sql
+++ src/main/resources/liquibase/scripts/faculties.sql
@@ -1,5 +1,3 @@
 -- liquibase formatted sql
 -- changeset mvasechko:2
-create index faculty_name_index on faculty (name);
--- changeset mvasechko:3
-create index faculty_color_index on faculty (color);
+create index faculty_name_color_index on faculty (name, color);
